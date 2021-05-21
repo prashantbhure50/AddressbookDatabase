469e5e949e1abd2d36b8fd91accaea1406253508
Prashantbhure50 <prashantbhure50@gmail.com>
2021-05-21 23:50:07 +0530

    [Prashant] Values Inserted Into Table

--- AddressbookData.sql
+++ AddressbookData.sql
@@ -11,5 +11,13 @@ City varchar(50) not null,
 PhoneNo varchar(100)not null,
 Zip varchar(50)not null,
 Email varchar(100)not null,
-name varchar(100) not null
 )
+
+select * from AddresssbookDatabase
+
+insert into AddresssbookDatabase values
+('Alok', 'Bhure', 'Tifra',' Bilaspur',' Chhattisgarh', '7000593588', '495223', 'Aloktbhure50@gmail.com'),
+('Prashant', 'Bhure', 'Trifa', 'Bilaspur', 'Chhattisgarh', '8965940302', '623456', 'prashantbhure@gmail.com'),
+('Ashish', 'Gupta', 'BTM', 'Bengaluru', 'Karanataka', '7000545388', '560076', 'AshishGupta@gmail.com'),
+('Manish', 'Sharma', 'Andheri', 'Mumbai', 'Maharastra', '7456732456','63456', 'Manishsharma@gmail.com'),
+('Shahshank', 'Gupta', 'Nirakia', 'korba', 'Chhattisgarh', '9874920382', '489223', 'Shashankgupta@gmail.com')

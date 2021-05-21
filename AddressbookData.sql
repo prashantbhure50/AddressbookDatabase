1e8488c9d0834ca4b7d2ab8ab64d32b7f3b92ea1
Prashantbhure50 <prashantbhure50@gmail.com>
2021-05-21 23:37:47 +0530

    [Prashant] table is Created

--- AddressbookData.sql
+++ AddressbookData.sql
@@ -1,2 +1,15 @@
 create database AddresssbookDatabase
 use AddresssbookDatabase;
+select DB_NAME()
+
+create table AddresssbookDatabase(
+FirstName varchar(100) not null,
+LastName varchar(100) not null,
+Addressdetail varchar(100) not null,
+State varchar(50) not null,
+City varchar(50) not null,
+PhoneNo varchar(100)not null,
+Zip varchar(50)not null,
+Email varchar(100)not null,
+name varchar(100) not null
+)
